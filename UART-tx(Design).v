1178a7f87205d38733e008751581abda6d4e05b3
Manish Gollapally <manishgollapally@gmail.com>
2026-01-25 11:23:31 +0530

    Add files via upload

---
+++ UART-tx(Design).v
@@ -0,0 +1,139 @@
+
+//DESIGN
+module uart_tx(
+    input clk,
+    input [7:0] data_in,
+    input rx,
+    input tx_start,
+    output reg tx = 1,
+    output reg [7:0] data_out,
+    output active
+);
+    parameter IDLE = 2'b00;
+    parameter START = 2'b01;
+    parameter DATA = 2'b10;
+    parameter STOP = 2'b11;
+
+    reg [1:0] tx_state = IDLE;
+    reg [1:0] rx_state = IDLE;
+    reg [12:0] tx_cnt = 0;
+    reg [12:0] rx_cnt = 0;
+    reg [2:0] tx_bit_index = 0;
+    reg [2:0] rx_bit_index = 0;
+    wire tx_en;
+    wire rx_en;
+    reg tx_busy = 0;
+    reg rx_busy = 0;
+
+    assign active = tx_busy || rx_busy;
+
+    parameter clk_freq = 50000000;
+    parameter baud_rate = 9600;
+    parameter clk_per_bit = clk_freq/baud_rate;
+    parameter clk_half_bit = clk_per_bit/2;
+
+    always @(posedge clk) begin
+        if (tx_busy==0)
+            tx_cnt <= 0;
+        else begin
+            if(tx_cnt==clk_per_bit)
+                tx_cnt <= 0;
+            else
+                tx_cnt <= tx_cnt + 1;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (rx_busy==0 || (rx_state==START && rx_cnt==clk_half_bit))
+            rx_cnt <= 0;
+        else begin
+            if (rx_cnt==clk_per_bit)
+                rx_cnt <= 0;
+            else
+                rx_cnt <= rx_cnt + 1;
+        end
+    end
+
+    assign tx_en = (tx_cnt==clk_per_bit-1);
+    assign rx_en = (rx_cnt == clk_per_bit-1);
+
+    always @(posedge clk) begin
+        case (tx_state)
+            IDLE: begin
+                tx <= 1;
+                tx_bit_index <= 0;
+
+                if (tx_start) begin
+                    tx_busy<=1;
+                    tx_state<=START;
+                end
+            end
+
+            START: begin
+                tx<=0;
+                if (tx_en) begin
+                    tx_state<=DATA;
+                end
+            end
+
+            DATA: begin
+                tx <= data_in[tx_bit_index];
+                if (tx_en) begin
+                    if (tx_bit_index==7) begin
+                        tx_bit_index <= 0;
+                        tx_state <= STOP;
+                    end
+                    else
+                        tx_bit_index <= tx_bit_index + 1;
+                end
+            end
+
+            STOP: begin
+                tx<=1;
+                if (tx_en) begin
+                    tx_busy <= 0;
+                    tx_state <= IDLE;
+                end
+            end
+
+        endcase
+    end
+
+    always @(posedge clk) begin
+        case (rx_state) 
+            IDLE: begin
+                if (rx==0) begin
+                    rx_busy <= 1;
+                    rx_state <= START;
+                end
+            end
+
+            START: begin
+                if (rx_cnt==clk_half_bit) begin
+                    rx_state <= DATA;
+                end
+            end
+
+            DATA: begin
+                if (rx_en) begin
+                    data_out[rx_bit_index] <= rx;
+                    if (rx_bit_index==7) begin
+                        rx_bit_index <= 0;
+                        rx_state <= STOP;
+                    end
+                    else
+                        rx_bit_index <= rx_bit_index + 1;
+                end
+            end
+
+            STOP: begin
+                if (rx_en) begin
+                    rx_busy <= 0;
+                    rx_state <= IDLE;
+                end
+            end
+
+        endcase
+    end
+
+endmodule
---
+++ UART-tx(Testbench).v
@@ -0,0 +1,56 @@
+
+//TESTBENCH
+module uart_tb;
+    reg r_clk=0;
+    reg [7:0] r_data_in=0;
+    reg r_tx_start=0;
+    wire w_loopback;
+    wire w_tx;
+    wire [7:0] w_data_out;
+    wire w_active;
+
+    uart_tx UUT(
+        .clk(r_clk),
+        .data_in(r_data_in),
+        .rx(w_loopback),
+        .tx_start(r_tx_start),
+        .tx(w_loopback),
+        .data_out(w_data_out),
+        .active(w_active)
+    );
+    always #10 r_clk=~r_clk;
+
+    initial begin
+        $dumpfile("uart_waves.vcd");
+        $dumpvars(0,uart_tb);
+
+        r_clk = 0;
+        r_data_in = 8'h00;
+        r_tx_start = 0;
+
+        #100;
+        r_data_in = 8'b10100110;
+        r_tx_start = 1;
+        #20;
+        r_tx_start = 0;
+        #100;
+        wait(w_active==1);
+        wait(w_active==0);
+        #100;
+        if(w_data_out==8'b10100110) begin
+            $display("Success! Input = %b | Output = %b",r_data_in,w_data_out);
+        end
+        else begin
+            $display("Failed!");
+        end
+
+        #20; $finish;
+    end
+
+    initial begin
+        #2000000; 
+        $display("ERROR: Simulation timed out! Stuck in wait loop.");
+        $finish; 
+    end  
+
+endmodule
